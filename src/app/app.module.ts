4ccdc93201c9cd689068c87a3e27c987578a6c7e
Nikita.Semkin <Nikita.Semkin@e-mundo.de>
2023-10-19 10:18:17 +0200

    Add global styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookFormComponent } from './components/book/book-form/book-form.component';
-import { BookListComponent } from './book-list/book-list.component';
+import { BookListComponent } from './components/book/book-list/book-list.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
---
+++ src/app/components/book/book-list/book-list.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {Book} from "../../../model/book.model";
+import {BookService} from "../../../services/book.service";
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+  books: Book[] = [];
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.bookService.getBooks().subscribe((books) => {
+      this.books = books;
+    });
+  }
+}
