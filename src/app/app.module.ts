0ed911e50cc17e39473924469bbed8874a025838
Nikita.Semkin <Nikita.Semkin@e-mundo.de>
2023-10-19 09:59:02 +0200

    Initialize Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookFormComponent } from './components/book/book-form/book-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/model/book.model.ts
@@ -0,0 +1,8 @@
+export interface Book {
+  name: string;
+  price: number;
+  author: string;
+  date: string;
+  language: string;
+  genre: string;
+}
